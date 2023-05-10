2788efc61775724706eac81b07464ffdda65a77a
ahmet <ahmet55uluisik05@gmail.com>
2023-05-10 13:16:12 +0300

    finished project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
 import "./App.css";
-
+import BannerImage from "./assets/banner.jpg";
 function App() {
-  return <div className="App">Welcome</div>;
+  return (
+    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+      <div className="headerContainer">
+        <h1>Burger 33</h1>
+        <p>
+          Delicious Hamburgers <br /> with 33 Spicy Mix
+        </p>
+        <Link to="/menu">
+          <button>Order Now</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,10 +16,11 @@ root.render(
       <Navbar />
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/" element={<App />} />
-        <Route path="/" element={<App />} />
-        <Route path="/" element={<App />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
